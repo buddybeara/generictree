0465430cade06dd29331a4becffd855b58fe1775
buddybeara <108195819+buddybeara@users.noreply.github.com>
2023-05-07 14:26:05 -0700

    please work
    j

--- js/layers.js
+++ js/layers.js
@@ -185,7 +185,7 @@ addLayer("po", {
 		points: new Decimal(0),
     }},
     color: "#FFFFFF",
-    requires: new Decimal(1e12), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1e100), // Can be a function that takes requirement increases into account
     resource: "positive", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
@@ -225,7 +225,7 @@ addLayer("n", {
 		points: new Decimal(0),
     }},
     color: "#000000",
-    requires: new Decimal(1e12), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1e100), // Can be a function that takes requirement increases into account
     resource: "negative", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
@@ -258,7 +258,7 @@ addLayer("n", {
 addLayer("i", {
 		branches: ['e','u'],
     name: "i", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "∞", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 4, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
